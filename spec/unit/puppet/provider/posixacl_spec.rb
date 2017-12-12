7af30364fc3a553129664f3ab855c7fd5ccc39d8
Alexander Fisher <alex@linfratech.co.uk>
2017-12-12 21:27:03 +0000

    Rubocop auto fixes (#37)
    * Rubocop: Fix Style/HashSyntax
    
    * Rubocop: Fix Layout/SpaceAroundBlockParameters
    
    * Rubocop: Fix Layout/EmptyLinesAroundBlockBody
    
    * Rubocop: Fix Style/StringLiterals
    
    * Rubocop: Fix Layout/SpaceAroundOperators
    
    * Rubocop: Fix Style/TrailingCommaInLiteral
    
    * Rubocop: Fix Layout/SpaceAfterComma
    
    * Rubocop: Fix Layout/LeadingCommentSpace
    
    * Rubocop: Fix Layout/CaseIndentation
    
    * Rubocop: Fix Style/RedundantReturn
    
    * Rubocop: Fix Style/CollectionMethods
    
    * Rubocop: Fix Style/AndOr
    
    * Rubocop: Fix Style/BlockDelimiters
    
    * Rubocop: Fix Style/NegatedIf
    
    * Rubocop: Fix Style/MultilineIfThen
    
    * Rubocop: Fix Lint/UnusedBlockArgument
    
    * Rubocop: Fix Style/RegexpLiteral
    
    * Rubocop: Fix Style/IfUnlessModifier
    
    * Rubocop: Fix RSpec/ExampleWording
    
    * Rubocop: Fix Style/ZeroLengthPredicate
    
    * Rubocop: Fix Style/ParenthesesAroundCondition
    
    * Rubocop: Fix Layout/IndentArray
    
    * Rubocop: Fix Style/MethodCallWithoutArgsParentheses
    
    * Rubocop: Fix Layout/EmptyLines
    
    * Rubocop: Fix Style/InverseMethods
    
    * Rubocop: Fix Lint/UnusedMethodArgument

--- spec/unit/puppet/provider/posixacl_spec.rb
+++ spec/unit/puppet/provider/posixacl_spec.rb
@@ -4,24 +4,23 @@ require 'rspec/mocks'
 provider_class = Puppet::Type.type(:posix_acl).provider(:posixacl)
 
 describe provider_class do
-
   it 'declares a getfacl command' do
-    expect{
+    expect do
       provider_class.command :getfacl
-    }.not_to raise_error
+    end.not_to raise_error
   end
   it 'declares a setfacl command' do
-    expect{
+    expect do
       provider_class.command :setfacl
-    }.not_to raise_error
+    end.not_to raise_error
   end
   it 'encodes spaces in group names' do
     RSpec::Mocks.with_temporary_scope do
       Puppet::Type.stubs(:getfacl).returns("group:test group:rwx\n")
       File.stubs(:exist?).returns(true)
-      expect{
+      expect do
         provider_class.command :permission
-      } == ['group:test\040group:rwx']
+      end == ['group:test\040group:rwx']
     end
   end
 end
--- spec/unit/puppet/type/acl_spec.rb
+++ spec/unit/puppet/type/acl_spec.rb
@@ -2,130 +2,129 @@ require 'spec_helper'
 
 acl_type = Puppet::Type.type(:posix_acl)
 
-
 describe acl_type do
   context 'when not setting parameters' do
-    it 'should fail without permissions' do
-      expect{
-        acl_type.new :name => '/tmp/foo'
-      }.to raise_error
+    it 'fails without permissions' do
+      expect do
+        acl_type.new name: '/tmp/foo'
+      end.to raise_error
     end
   end
   context 'when setting parameters' do
-    it 'should work with a correct permission parameter' do
-      resource = acl_type.new :name => '/tmp/foo', :permission => ['user:root:rwx']
+    it 'works with a correct permission parameter' do
+      resource = acl_type.new name: '/tmp/foo', permission: ['user:root:rwx']
       expect(resource[:name]).to eq('/tmp/foo')
       expect(resource[:permission]).to eq(['user:root:rwx'])
     end
-    it 'should convert a permission string to an array' do
-      resource = acl_type.new :name => '/tmp/foo', :permission => 'user:root:rwx'
+    it 'converts a permission string to an array' do
+      resource = acl_type.new name: '/tmp/foo', permission: 'user:root:rwx'
       expect(resource[:name]).to eq('/tmp/foo')
       expect(resource[:permission]).to eq(['user:root:rwx'])
     end
-    it 'should convert the u: shorcut to user:' do
-      resource = acl_type.new :name => '/tmp/foo', :permission => ['u:root:rwx']
+    it 'converts the u: shorcut to user:' do
+      resource = acl_type.new name: '/tmp/foo', permission: ['u:root:rwx']
       expect(resource[:name]).to eq('/tmp/foo')
       expect(resource[:permission]).to eq(['user:root:rwx'])
     end
-    it 'should convert the g: shorcut to group:' do
-      resource = acl_type.new :name => '/tmp/foo', :permission => ['g:root:rwx']
+    it 'converts the g: shorcut to group:' do
+      resource = acl_type.new name: '/tmp/foo', permission: ['g:root:rwx']
       expect(resource[:name]).to eq('/tmp/foo')
       expect(resource[:permission]).to eq(['group:root:rwx'])
     end
-    it 'should convert the m: shorcut to mask:' do
-      resource = acl_type.new :name => '/tmp/foo', :permission => ['m::rwx']
+    it 'converts the m: shorcut to mask:' do
+      resource = acl_type.new name: '/tmp/foo', permission: ['m::rwx']
       expect(resource[:name]).to eq('/tmp/foo')
       expect(resource[:permission]).to eq(['mask::rwx'])
     end
-    it 'should convert the o: shorcut to other:' do
-      resource = acl_type.new :name => '/tmp/foo', :permission => ['o::rwx']
+    it 'converts the o: shorcut to other:' do
+      resource = acl_type.new name: '/tmp/foo', permission: ['o::rwx']
       expect(resource[:name]).to eq('/tmp/foo')
       expect(resource[:permission]).to eq(['other::rwx'])
     end
-    it 'should have the "set" action by default' do
-      resource = acl_type.new :name => '/tmp/foo', :permission => ['o::rwx']
+    it 'has the "set" action by default' do
+      resource = acl_type.new name: '/tmp/foo', permission: ['o::rwx']
       expect(resource[:name]).to eq('/tmp/foo')
       expect(resource[:action]).to eq(:set)
     end
-    it 'should accept an action "set"' do
-      resource = acl_type.new :name => '/tmp/foo', :permission => ['o::rwx'], :action => :set
+    it 'accepts an action "set"' do
+      resource = acl_type.new name: '/tmp/foo', permission: ['o::rwx'], action: :set
       expect(resource[:name]).to eq('/tmp/foo')
       expect(resource[:action]).to eq(:set)
     end
-    it 'should accept an action "purge"' do
-      resource = acl_type.new :name => '/tmp/foo', :permission => ['o::rwx'], :action => :purge
+    it 'accepts an action "purge"' do
+      resource = acl_type.new name: '/tmp/foo', permission: ['o::rwx'], action: :purge
       expect(resource[:name]).to eq('/tmp/foo')
       expect(resource[:action]).to eq(:purge)
     end
-    it 'should accept an action "unset"' do
-      resource = acl_type.new :name => '/tmp/foo', :permission => ['o::rwx'], :action => :unset
+    it 'accepts an action "unset"' do
+      resource = acl_type.new name: '/tmp/foo', permission: ['o::rwx'], action: :unset
       expect(resource[:name]).to eq('/tmp/foo')
       expect(resource[:action]).to eq(:unset)
     end
-    it 'should accept an action "exact"' do
-      resource = acl_type.new :name => '/tmp/foo', :permission => ['o::rwx'], :action => :exact
+    it 'accepts an action "exact"' do
+      resource = acl_type.new name: '/tmp/foo', permission: ['o::rwx'], action: :exact
       expect(resource[:name]).to eq('/tmp/foo')
       expect(resource[:action]).to eq(:exact)
     end
-    it 'should have path as namevar' do
-      resource = acl_type.new :name => '/tmp/foo', :permission => ['o::rwx']
+    it 'has path as namevar' do
+      resource = acl_type.new name: '/tmp/foo', permission: ['o::rwx']
       expect(resource[:name]).to eq('/tmp/foo')
       expect(resource[:path]).to eq(resource[:name])
     end
-    it 'should accept a path parameter' do
-      resource = acl_type.new :path => '/tmp/foo', :permission => ['o::rwx'], :action => :exact
+    it 'accepts a path parameter' do
+      resource = acl_type.new path: '/tmp/foo', permission: ['o::rwx'], action: :exact
       expect(resource[:path]).to eq('/tmp/foo')
       expect(resource[:name]).to eq(resource[:path])
     end
-    it 'should not be recursive by default' do
-      resource = acl_type.new :name => '/tmp/foo', :permission => ['o::rwx']
+    it 'is not recursive by default' do
+      resource = acl_type.new name: '/tmp/foo', permission: ['o::rwx']
       expect(resource[:name]).to eq('/tmp/foo')
       expect(resource[:recursive]).to eq(:false)
     end
-    it 'should accept a recursive "true"' do
-      resource = acl_type.new :name => '/tmp/foo', :permission => ['o::rwx'], :recursive => true
+    it 'accepts a recursive "true"' do
+      resource = acl_type.new name: '/tmp/foo', permission: ['o::rwx'], recursive: true
       expect(resource[:name]).to eq('/tmp/foo')
       expect(resource[:recursive]).to eq(:true)
     end
-    it 'should accept a recurse "false"' do
-      resource = acl_type.new :name => '/tmp/foo', :permission => ['o::rwx'], :recursive => false
+    it 'accepts a recurse "false"' do
+      resource = acl_type.new name: '/tmp/foo', permission: ['o::rwx'], recursive: false
       expect(resource[:name]).to eq('/tmp/foo')
       expect(resource[:recursive]).to eq(:false)
     end
-    it 'should get recursemode lazy by default' do
-      resource = acl_type.new :name => '/tmp/foo', :permission => ['o::rwx']
+    it 'gets recursemode lazy by default' do
+      resource = acl_type.new name: '/tmp/foo', permission: ['o::rwx']
       expect(resource[:name]).to eq('/tmp/foo')
       expect(resource[:recursemode]).to eq(:lazy)
     end
-    it 'should accept a recursemode deep' do
-      resource = acl_type.new :name => '/tmp/foo', :permission => ['o::rwx'], :recursemode => 'deep'
+    it 'accepts a recursemode deep' do
+      resource = acl_type.new name: '/tmp/foo', permission: ['o::rwx'], recursemode: 'deep'
       expect(resource[:name]).to eq('/tmp/foo')
       expect(resource[:recursemode]).to eq(:deep)
     end
-    it 'should accept a recursemode lazy' do
-      resource = acl_type.new :name => '/tmp/foo', :permission => ['o::rwx'], :recursemode => :lazy
+    it 'accepts a recursemode lazy' do
+      resource = acl_type.new name: '/tmp/foo', permission: ['o::rwx'], recursemode: :lazy
       expect(resource[:name]).to eq('/tmp/foo')
       expect(resource[:recursemode]).to eq(:lazy)
     end
-    it 'should fail with a wrong action' do
-      expect{
-        acl_type.new :name => '/tmp/foo', :permission => ['o::rwx'], :action => :xset
-      }.to raise_error
+    it 'fails with a wrong action' do
+      expect do
+        acl_type.new name: '/tmp/foo', permission: ['o::rwx'], action: :xset
+      end.to raise_error
     end
-    it 'should fail with a wrong recurselimit' do
-      expect{
-        acl_type.new :name => '/tmp/foo', :permission => ['o::rwx'], :recurselimit => :a
-      }.to raise_error
+    it 'fails with a wrong recurselimit' do
+      expect do
+        acl_type.new name: '/tmp/foo', permission: ['o::rwx'], recurselimit: :a
+      end.to raise_error
     end
-    it 'should fail with a wrong first argument' do
-      expect{
-        acl_type.new :name => '/tmp/foo', :permission => ['wrong::rwx']
-      }.to raise_error
+    it 'fails with a wrong first argument' do
+      expect do
+        acl_type.new name: '/tmp/foo', permission: ['wrong::rwx']
+      end.to raise_error
     end
-    it 'should fail with a wrong last argument' do
-      expect{
-        acl_type.new :name => '/tmp/foo', :permission => ['user::-_-']
-      }.to raise_error
+    it 'fails with a wrong last argument' do
+      expect do
+        acl_type.new name: '/tmp/foo', permission: ['user::-_-']
+      end.to raise_error
     end
   end
 
@@ -134,23 +133,22 @@ describe acl_type do
     advanced_perms = ['user:foo:rwx', 'group:foo:rwx', 'default:user:foo:---']
     advanced_perms_results = ['user:foo:rwx', 'group:foo:rwx']
     mysql_perms = [
-          "user:mysql:rwx",
-          "d:user:mysql:rw",
-          "mask::rwx",
+      'user:mysql:rwx',
+      'd:user:mysql:rw',
+      'mask::rwx'
     ]
     mysql_perms_results = [
-          "user:mysql:rwx",
-          "mask::rwx",
+      'user:mysql:rwx',
+      'mask::rwx'
     ]
-    it 'should not do anything with no defaults' do
+    it 'does not do anything with no defaults' do
       expect(acl_type.pick_default_perms(basic_perms)).to match_array(basic_perms)
     end
-    it 'should remove defaults' do
+    it 'removes defaults' do
       expect(acl_type.pick_default_perms(advanced_perms)).to match_array(advanced_perms_results)
     end
-    it 'should remove defaults with d:' do
+    it 'removes defaults with d:' do
       expect(acl_type.pick_default_perms(mysql_perms)).to match_array(mysql_perms_results)
     end
   end
-
 end
