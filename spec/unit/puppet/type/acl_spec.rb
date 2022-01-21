voxpupuli/puppet-posix_acl

commit a252009959434a6eb29400368d112fdfcdad927d

rubocop: Fix Style/FrozenStringLiteralComment

lib/puppet/type/posix_acl.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'set'
 require 'pathname'
 require 'English'

spec/unit/puppet/type/acl_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 acl_type = Puppet::Type.type(:posix_acl)
 

lib/puppet/provider/posix_acl/genericacl.rb
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+
 Puppet::Type.type(:posix_acl).provide(:genericacl, parent: Puppet::Provider) do
 end

lib/puppet/provider/posix_acl/posixacl.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Puppet::Type.type(:posix_acl).provide(:posixacl, parent: Puppet::Provider) do
   desc 'Provide posix 1e acl functions using posix getfacl/setfacl commands'
 

spec/unit/puppet/provider/posixacl_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require 'rspec/mocks'
 

commit 6f2d36aa24fac5f22e3c45399e153984f11102be

Properly handle frozen strings
This rewrites the ACL parsing in the type.

First of all, it renames variables from a single letter to multiple so
it properly describes what they are.

It also fixes the octal parsing, which always resulted in rwx.

More importantly, it fixes the frozen string behavior. It does this by
using += instead of << and avoiding sub!.

Fixes: a252009959434a6eb29400368d112fdfcdad927d

lib/puppet/type/posix_acl.rb
@@ -208,46 +208,45 @@ Puppet::Type.newtype(:posix_acl) do
 
     # Munge into normalised form
     munge do |acl|
-      r = ''
+      result = ''
       a = acl.split ':', -1 # -1 keeps trailing empty fields.
       raise ArgumentError, "Too few fields.  At least 3 required, got #{a.length}." if a.length < 3
       raise ArgumentError, "Too many fields.  At most 4 allowed, got #{a.length}."  if a.length > 4
 
       if a.length == 4
-        d = a.shift
-        raise ArgumentError, %(First field of 4 must be "d" or "default", got "#{d}".) unless %w[d default].include?(d)
+        default, type, who, perms = a
+        raise ArgumentError, %(First field of 4 must be "d" or "default", got "#{default}".) unless %w[d default].include?(default)
 
-        r += 'default:'
-      end
-      t = a.shift # Copy the type.
-      r += case t
-           when 'u', 'user'
-             'user:'
-           when 'g', 'group'
-             'group:'
-           when 'o', 'other'
-             'other:'
-           when 'm', 'mask'
-             'mask:'
-           else
-             raise ArgumentError, %(Unknown type "#{t}", expected "user", "group", "other" or "mask".)
-           end
-      r << "#{a.shift}:" # Copy the "who".
-      p = a.shift
-      if p =~ %r{[0-7]}
-        p = p.oct
-        r << (p | 4 ? 'r' : '-')
-        r << (p | 2 ? 'w' : '-')
-        r << (p | 1 ? 'x' : '-')
+        result += 'default:'
       else
-        # Not the most efficient but checks for multiple and invalid chars.
-        s = p.tr '-', ''
-        r << (s.sub!('r', '') ? 'r' : '-')
-        r << (s.sub!('w', '') ? 'w' : '-')
-        r << (s.sub!(%r{x}i, '') ? $LAST_MATCH_INFO.to_s : '-')
-        raise ArgumentError, %(Invalid permission set "#{p}".) unless s.empty?
+        type, who, perms = a
       end
-      r
+
+      result += case type
+                when 'u', 'user'
+                  'user:'
+                when 'g', 'group'
+                  'group:'
+                when 'o', 'other'
+                  'other:'
+                when 'm', 'mask'
+                  'mask:'
+                else
+                  raise ArgumentError, %(Unknown type "#{t}", expected "user", "group", "other" or "mask".)
+                end
+      result += "#{who}:"
+      if perms.match?(%r{^[0-7]$})
+        octal = perms.oct
+        result += "#{octal & 4 == 4 ? 'r' : '-'}#{octal & 2 == 2 ? 'w' : '-'}#{octal & 1 == 1 ? 'x' : '-'}"
+      else
+        match = %r{^(?<read>r)?(?<write>w)?(?<execute>[xX])?$}.match(perms.tr('-', ''))
+        raise ArgumentError, %(Invalid permission set "#{p}".) unless match
+
+        %w[read write execute].each do |perm|
+          result += match[perm] || '-'
+        end
+      end
+      result
     end
   end
 

spec/unit/puppet/type/acl_spec.rb
@@ -25,6 +25,12 @@ describe acl_type do
       expect(resource[:permission]).to eq(['default:user:root:rwx'])
     end
 
+    it 'works with octal 0 permission parameter' do
+      resource = acl_type.new name: '/tmp/foo', permission: ['user:root:0']
+      expect(resource[:name]).to eq('/tmp/foo')
+      expect(resource[:permission]).to eq(['user:root:---'])
+    end
+
     it 'works with octal 7 permission parameter' do
       resource = acl_type.new name: '/tmp/foo', permission: ['user:root:7']
       expect(resource[:name]).to eq('/tmp/foo')